4db3ed6ad7d377289924ab53e64836a258f13309
Anh Tuan Nguyen <anhtuan@yahoo-inc.com>
2014-09-22 23:31:19 -0700

    -redid auto layout: fixed layout and cell height -new search on text change -removed dead code

--- Yelp/ViewController.swift
+++ Yelp/ViewController.swift
@@ -25,6 +25,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.init(coder: aDecoder)
     }
     
+    @IBAction func editingChanged(sender: AnyObject) {
+        search(nil, radius: nil, deals: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -32,7 +36,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         tableView.delegate = self
         tableView.dataSource = self
-//        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.rowHeight = UITableViewAutomaticDimension
         
         search(nil, radius: nil, deals: nil)
     }
@@ -74,8 +78,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let restaurant = restaurants[indexPath.row]
         let name = restaurant["name"] as String
         cell.restaurantName.text = String(indexPath.row + 1) + ". " + name
-        // test auto layout
-//        cell.restaurantName.text = cell.restaurantName.text! + name + name
         if (restaurant["distance"] != nil) {
             cell.distance.text = restaurant["distance"] as String
         } else {
@@ -99,65 +101,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var imageUrl = restaurant["image_url"] as String
         cell.restaurantImage.setImageWithURL(NSURL.URLWithString(imageUrl))
         
-//        println(cell.restaurantType.frame.minY)
-//        println(cell.detailsView.frame.minY)
-        
         return cell as UITableViewCell
     }
-
-//    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-//        var cell = tableView.dequeueReusableCellWithIdentifier("RestaurantCell") as RestaurantTableViewCell
-//        let restaurant = restaurants[indexPath.row]
-//        cell.restaurantName.text = String(indexPath.row + 1) + ". " + (restaurant["name"] as String)
-//        if (restaurant["distance"] != nil) {
-//            cell.distance.text = restaurant["distance"] as String
-//        } else {
-//            cell.distance.text = "? mi"
-//        }
-//        if (restaurant["rating"] != nil) {
-//            let rating = restaurant["rating"] as Int
-//            cell.ratings.text = String(rating) + "star(s)"
-//        } else {
-//            cell.ratings.text = "? stars"
-//        }
-//        let reviewCount = restaurant["review_count"] as Int
-//        cell.reviewCount.text =  String(reviewCount) + " review(s)"
-//        let location = restaurant["location"] as NSDictionary
-//        let address = location["address"] as [String]
-//        var strAddress = ""
-//        for word in address {
-//            strAddress += word
-//        }
-//        cell.address.text = strAddress
-//        var imageUrl = restaurant["image_url"] as String
-//        cell.restaurantImage.setImageWithURL(NSURL.URLWithString(imageUrl))
-//
-//        // layout bottom details view
-////        cell.detailsView.layoutSubviews()
-////        cell.detailsView.frame.size.height = cell.restaurantType.frame.maxY
-//        
-//        let size = cell.systemLayoutSizeFittingSize(tableView.frame.size)
-//        
-////        let size = cell.systemLayoutSizeFittingSize(tableView.frame.size, withHorizontalFittingPriority: UILayoutPriorityRequired, verticalFittingPriority: UILayoutPriorityFittingSizeLevel)
-//        let s = cell.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
-//        
-////        cell.layoutSubviews()
-////        cell.frame.size.height = cell.detailsView.frame.maxY
-//
-////        println("type \(cell.restaurantType.frame.maxY)")
-////        println("cell \(cell.frame.height)")
-//        
-////        println(cell.frame.width)
-////        println("table size \(tableView.frame.size)")
-//        println(size)
-//        println(s)
-//        return 88
-//    }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-//        let navController = segue.destinationViewController as UINavigationController
-//        let viewC = navController.viewControllers[0] as FiltersViewController
-//        viewC.delegate = self
         var destinationViewController = segue.destinationViewController as FiltersViewController
         destinationViewController.delegate = self
     }
